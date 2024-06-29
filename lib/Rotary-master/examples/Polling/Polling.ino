6f87e51fa80af5c0e9e54669cfd48f9c7173d7e2
Gabriel Cor <gabrielcor@hotmail.com>
2024-06-29 18:47:54 -0400

    rotary working

--- lib/Rotary-master/examples/Polling/Polling.ino
+++ lib/Rotary-master/examples/Polling/Polling.ino
@@ -5,14 +5,12 @@
     * encoder pin A to Arduino pin 2
     * encoder pin B to Arduino pin 3
     * encoder ground pin to ground (GND)
-       * Clock: 35
-    * Data: 32 
+    
 */
-#include <Arduino.h>
+
 #include <Rotary.h>
 
-int valor = 0;
-Rotary r = Rotary(32, 35);
+Rotary r = Rotary(2, 3);
 
 void setup() {
   Serial.begin(9600);
@@ -22,15 +20,6 @@ void setup() {
 void loop() {
   unsigned char result = r.process();
   if (result) {
-    if (result == DIR_CW)
-    {
-      valor++;
-    }
-    else
-    {
-      valor--;
-    }
-    // Serial.println(result == DIR_CW ? "Right" : "Left");
-    Serial.println(valor);
+    Serial.println(result == DIR_CW ? "Right" : "Left");
   }
 }
